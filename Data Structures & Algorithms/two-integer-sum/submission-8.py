75f8f7df35e2f377d8f3fe48cddb8ff8d5cfcbef
Shreyas Chaudhary <shreyaschaudhary3@gmail.com>
2026-04-21 18:07:13 -0500

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for x in range(len(nums)):
+            for y in range(len(nums)):
+                if nums[x]+nums[y]==target and x!=y:
+                    return [x,y]
+        
