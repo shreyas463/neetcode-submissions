3dd1cd29ccc791524806d3639228adf12999e2ae
Shreyas Chaudhary <shreyaschaudhary3@gmail.com>
2026-04-24 23:44:25 -0500

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l,r=0,1
+        maxProfit=0
+        while r<len(prices):
+            if prices[l]<prices[r]: #checking if profitable transaction
+                profit=prices[r]-prices[l]
+                maxProfit=max(maxProfit,profit)
+            else:#if not profitable
+                l=r 
+            r+=1
+        return maxProfit
+                
+            
+
+
+       
+        
+
+        
+        
