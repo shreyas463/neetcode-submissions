3fa2ca25e08e215374f29aee6974316a2d849b24
Shreyas Chaudhary <shreyaschaudhary3@gmail.com>
2026-07-06 22:41:57 -0500

    Update: two-integer-sum - submission-32

--- Data Structures & Algorithms/two-integer-sum/submission-32.py
+++ Data Structures & Algorithms/two-integer-sum/submission-32.py
@@ -1,12 +1,32 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        #prerequisites are lenght of two strings must be equal and indiv char too
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hmap={
 
-        if len(s)!= len(t):
-            return False
+        }
+        for i in range(len(nums)):
+            current = nums[i]
+
+            if current in hmap:
+                return [hmap[current],i]
+            
+            needed=target-current
+            hmap[needed]=i
+
+
+    #         class Solution:
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     hmap={
+
+    #     }
+    #     for i in range(len(nums)):
+    #         current =nums[i] #3 , 4
+
+    #         if current in hmap: #3 in hmap - no , 4 in map - yes
+    #             return [hmap[current],i] #so return back 0 (position), i value which is 1
+
+    #         needed=target-current #7-3=4
+    #         hmap[needed]=i # 4 at 0
+
         
-        for i in s: #going through each char cnt in strings
-            if s.count(i)!=t.count(i):
-                return False
-        return True
+
         
