4b0de78ae75ab17d2ff28af707ffeb9f4c3a1d6d
Shreyas Chaudhary <shreyaschaudhary3@gmail.com>
2026-07-07 14:04:23 -0500

    Add: two-integer-sum - submission-33

---
+++ Data Structures & Algorithms/two-integer-sum/submission-33.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hmap={
+
+        }
+        for i in range(len(nums)):
+            current=nums[i]
+            if current in hmap:
+                return [hmap[current],i]
+            needed=target-current
+            hmap[needed]=i
