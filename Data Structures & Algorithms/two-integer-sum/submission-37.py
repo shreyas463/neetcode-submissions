8feaead4e0f41aa47c2018973c292153b9ba1885
Shreyas Chaudhary <shreyaschaudhary3@gmail.com>
2026-08-24 14:17:26 -0700

    Update: two-integer-sum - submission-37

--- Data Structures & Algorithms/two-integer-sum/submission-37.py
+++ Data Structures & Algorithms/two-integer-sum/submission-37.py
@@ -1,11 +1,15 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-        for i in s:
-            if s.count(i)!=t.count(i):
-                return False
-        return True
-        
-        #first we check lenght of two strings and then indiv char count, 
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hmap={}
+
+        for i in range(len(nums)):
+            current=nums[i]
+
+            if current in hmap:
+                return [hmap[current],i]
+            
+            needed=target-current 
+            hmap[needed]=i
+
+#so basically we started with an empty hashmap. then assigned the 1st no as current. then check if current is in hmap. if not we calculate the difference which is needed and make it the starting number. if suppose current is there in hmap, we return it along with its index value
         
