f5508168d528a58edf8c8605a98626e861127742
Shreyas Chaudhary <shreyaschaudhary3@gmail.com>
2026-08-26 15:25:40 -0500

    Add: minimum-difference-between-highest-and-lowest-of-k-scores - submission-1

---
+++ Data Structures & Algorithms/minimum-difference-between-highest-and-lowest-of-k-scores/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        min_score=float('inf')
+        n=len(nums)
+
+        for l in range(n-k+1): #gives range-01,2,3
+            r=l+k-1
+
+            diff=nums[r]-nums[l]
+            min_score=min(diff, min_score)
+
+        return min_score
+
+        
