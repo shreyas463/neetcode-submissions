9961ab8c52c2ec5632a3297297a4d33a7aa7bc97
Shreyas Chaudhary <shreyaschaudhary3@gmail.com>
2026-08-28 11:08:14 -0500

    Add: minimum-stack - submission-8

---
+++ Data Structures & Algorithms/minimum-stack/submission-8.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+
+    def push(self, val):
+        self.stack.append(val)
+
+        if not self.minstack or val <= self.minstack[-1]:
+            self.minstack.append(val)
+
+    def pop(self):
+        val = self.stack.pop()
+
+        if val == self.minstack[-1]:
+            self.minstack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.minstack[-1]
