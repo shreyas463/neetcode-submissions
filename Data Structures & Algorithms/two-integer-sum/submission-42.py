9519824857996e2225b4c5f189990bc5d3222d8b
Shreyas Chaudhary <shreyaschaudhary3@gmail.com>
2026-09-05 10:26:04 -0500

    Add: two-integer-sum - submission-42

---
+++ Data Structures & Algorithms/two-integer-sum/submission-42.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hmap={}
+
+        for i in range(len(nums)):
+            current=nums[i]
+
+            if current in hmap:
+                return [hmap[nums[i]],i]
+
+            needed=target-current
+            hmap[needed]=i
+        
+
+
+
+
+#so basically we started with an empty hashmap. then assigned the 1st no as current. then check if current is in hmap. if not we calculate the difference which is needed and make it the starting number. if suppose current is there in hmap, we return it along with its index value
+        
+        
