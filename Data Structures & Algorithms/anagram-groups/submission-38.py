1f7295ca4996485d2136f23f04b43c236ae73337
Shreyas Chaudhary <shreyaschaudhary3@gmail.com>
2026-09-14 20:33:07 -0500

    Add: anagram-groups - submission-38

---
+++ Data Structures & Algorithms/anagram-groups/submission-38.py
@@ -0,0 +1,23 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        ans=defaultdict(list)
+
+        for i in strs:
+            sorted_ans="".join(sorted(i))
+            ans[sorted_ans].append(i)
+        
+        return list(ans.values())
+        
+
+
+#         ans=defaultdict(list)
+# # pots -> 'o','p','s','t' ->opst -> ans[opst].append(pots)
+# #"opst": ["pots", "tops"]
+#         for i in strs:
+
+#             sorted_ans="".join(sorted(i))
+#             ans[sorted_ans].append(i)
+
+#         return list(ans.values())
+        
